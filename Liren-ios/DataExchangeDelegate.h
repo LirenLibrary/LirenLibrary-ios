006e65c338e0737d7ff91027b3fe09793e2ccb3a
xuehai <xhzeng@thoughtworks.com>
2012-12-12 10:10:48 +0800

    xuehai - added a data exchange protocol
    Controllers should use the delegate to exchange data, so they can
    weakly hold each other, avoid cycly retain

---
+++ Liren-ios/DataExchangeDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DataExchangeDelegate.h
+//  Liren-ios
+//
+//  Created by xuehai on 12/12/12.
+//  Copyright (c) 2012 com.thoughtworks.liren. All rights reserved.
+//
+//  The delegate for the data exchange between view controllers
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DataExchangeDelegate <NSObject>
+
+-(void)exchangeDatePut:(NSObject *)dataObject;
+
+@end
