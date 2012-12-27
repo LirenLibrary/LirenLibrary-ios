b91ef683543a46cd5e3e9b35e9715b4f15bb2388
xuehai <xhzeng@thoughtworks.com>
2012-12-27 14:30:26 +0800

    [Story21][xuehai] - Added the UI test

--- Frank/features/#21_Tap_to_Donate.feature
+++ Frank/features/#21_Tap_to_Donate.feature
@@ -13,3 +13,9 @@ Then I should be on the BookScanListViewController
 
 When I tap 返回 button
 Then I should be on the landing screen
+
+When I tap 记录 button
+Then I should be on the DonationListViewController
+
+When I tap 返回 button
+Then I should be on the landing screen
--- Frank/features/step_definitions/#21_Tap_to_Donate_steps.rb
+++ Frank/features/step_definitions/#21_Tap_to_Donate_steps.rb
@@ -1,23 +1,31 @@
-Then /^I should be on the landing screen$/ do
-	#检查是否有‘捐赠’按钮
-	check_element_exists "view view:'UIRoundedRectButton' marked:'\u6350\u8d60'"
+# coding: utf-8
 
-	#检查是否有'记录'按钮
-	check_element_exists "view view:'UIRoundedRectButton' marked:'\u8bb0\u5f55'"
+Then /^I should be on the landing screen$/ do
+	check_element_exists "view view:'UIRoundedRectButton' marked:'捐赠'"
+	check_element_exists "view view:'UIRoundedRectButton' marked:'记录'"
 end
 
-When /^I tap \u6350\u8d60 button$/ do
-	touch "view view:'UIRoundedRectButton' marked:'\u6350\u8d60'"
+When /^I tap 捐赠 button$/ do
+	touch "view view:'UIRoundedRectButton' marked:'捐赠'"
 	sleep(1)
 end
 
 Then /^I should be on the BookScanListViewController$/ do
-	check_element_exists "view view:'UIRoundedRectButton' marked:'\u626b\u63cf'"
+	check_element_exists "view view:'UIRoundedRectButton' marked:'扫描'"
 	check_element_exists "view view:'UITableView' marked:'Empty list'"
-	check_element_exists "view view:'UINavigationItemView' marked:'\u7acb\u4eba\u6350\u4e66'"
+	check_element_exists "view view:'UINavigationItemView' marked:'立人捐书'"
 end
 
-When /^I tap \u8fd4\u56de button$/ do
-	touch "view:'UINavigationItemButtonView' marked:'\u8fd4\u56de'"
+When /^I tap 返回 button$/ do
+	touch "view:'UINavigationItemButtonView' marked:'返回'"
 	sleep(1)
+end
+
+When /^I tap 记录 button$/ do
+	touch "view view:'UIRoundedRectButton' marked:'记录'"
+	sleep(2)
+end
+
+Then /^I should be on the DonationListViewController$/ do
+
 end
