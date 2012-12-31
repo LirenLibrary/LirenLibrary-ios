df8a18acaa0061b41a35a4db72b6d34f35529e7d
xuehai <xhzeng@thoughtworks.com>
2012-12-31 10:04:10 +0800

    [NA]-xuehai-Edited the UI test scripts

--- run-frank.sh
+++ run-frank.sh
@@ -5,7 +5,7 @@ rm -rf functional-test-reports
 RETVAL=$?
 #osascript -e 'tell app "iPhone Simulator" to quit'
 cd Frank
-cucumber -f junit -o ../functional-test-reports/ -t @Automated
+cucumber -f junit -o ../functional-test-reports/
 
 cd ..
 osascript -e 'tell app "iPhone Simulator" to quit'
